0601bade469c4f0fc87fec31034ca8ca61ae661a
gggordon <ggg_gilroy@yahoo.com>
2019-02-18 03:18:27 -0500

    Initial Commit

---
+++ src/main/java/jm/edu/utech/fruitbasketmone/FruitbasketmoneApplication.java
@@ -0,0 +1,13 @@
+package jm.edu.utech.fruitbasketmone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitbasketmoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruitbasketmoneApplication.class, args);
+	}
+
+}
