8bc001e612b5d94871459fad221cb8cc633cefb7
gggordon <ggg_gilroy@yahoo.com>
2019-02-25 08:36:53 -0500

    Included basket component

---
+++ src/main/resources/static/assets/components/basket/basket.component.js
@@ -0,0 +1,11 @@
+if(app){ //ensure app is defined
+	app.component('basket',{
+		templateUrl:'assets/components/basket/basket.component.html',
+		controller: [
+			'$scope',
+			function BasketController($scope){
+				
+			}
+		]
+	});
+}
---
+++ src/main/resources/static/assets/js/angular-app.js
@@ -0,0 +1,3 @@
+var app = angular.module('fruitbasket',['restangular']);
+
+//app.component()
