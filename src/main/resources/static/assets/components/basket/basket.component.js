1949df5a059913543fd56612b55f24640e28f984
gggordon <ggg_gilroy@yahoo.com>
2019-02-25 09:14:30 -0500

    Add to baskets

--- src/main/resources/static/assets/components/basket/basket.component.js
+++ src/main/resources/static/assets/components/basket/basket.component.js
@@ -1,9 +1,15 @@
 if(app){ //ensure app is defined
 	app.component('basket',{
 		templateUrl:'assets/components/basket/basket.component.html',
+		controllerAs: '$ctrl',
+		bindings:{
+			ownerName:'<'
+		},
 		controller: [
 			'$scope',
 			function BasketController($scope){
+				$scope.operationType = '';
+				var lastBasketId = 2;
 				$scope.baskets = [
 					{
 						id:1,
@@ -14,6 +20,29 @@ if(app){ //ensure app is defined
 						ownerName:"Tom Riddle"
 					}
 				];
+				
+				$scope.showAdd=function(){
+					$scope.operationType='Add';
+				}
+				
+				$scope.doAction=function(){
+					switch($scope.operationType){
+					case 'Add': addBasket();
+					}
+				}
+				
+				function addBasket(){
+					var ownerName = $scope.$ctrl.ownerName;
+					console.log("Adding ",ownerName);
+					$scope.baskets.push({
+						id : ++lastBasketId,
+						ownerName : ownerName
+					});
+					$scope.operationType='';
+					window.alert("Basket added successfully");
+					
+					
+				}
 			}
 		]
 	});
--- src/main/resources/static/assets/js/angular-app.js
+++ src/main/resources/static/assets/js/angular-app.js
@@ -1,3 +1,2 @@
 var app = angular.module('fruitbasket',['restangular']);
 
-//app.component()
